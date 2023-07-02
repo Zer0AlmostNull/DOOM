b0db1be5e0eced09e7c8caf1422366f2bc1c1d19
Wiktor Persak <62188864+Zer0AlmostNull@users.noreply.github.com>
2023-07-03 01:00:07 +0200

    Finished drawing library
    Its blazingly fast but no user-frienly at all. And its not safe to use

--- draw.cpp
+++ draw.cpp
@@ -2,6 +2,24 @@
 #include <algorithm>
 #include <math.h>
 
+// Even faster function for drawing vertical line
+// but y0 < y1 must be true
+void draw_line_vertical(Surface& surf, u16 x, u16 y0, u16 y1, u32 color)
+{
+    for (int y = y0; y <= y1; y++)
+    {
+        surf.buff[y * surf.width + x] = color;
+    }
+}
+
+void draw_line_horizontal(Surface& surf, u16 x0, u16 x1, u16 y, u32 color)
+{
+    for (int x = x0; x < x1; x++)
+    {
+        surf.buff[y * surf.width + x] = color;
+    }
+}
+
 void draw_line(Surface& surf, u16 x0, u16 y0, u16 x1, u16 y1, u32 color)
 {
     // check if the coordinates are out of bounds
@@ -44,3 +62,76 @@ void draw_line(Surface& surf, u16 x0, u16 y0, u16 x1, u16 y1, u32 color)
         }
     }
 }
+
+void draw_circle(Surface& surf, u16 x0, u16 y0, u16 radius, u32 color)
+{
+    int x = radius;
+    int y = 0;
+    int err = 0;
+
+    while (x >= y) {
+        surf.buff[((y0 + y) * surf.width) + (x0 + x)] = color;
+        surf.buff[((y0 + x) * surf.width) + (x0 + y)] = color;
+        surf.buff[((y0 - x) * surf.width) + (x0 + y)] = color;
+        surf.buff[((y0 - y) * surf.width) + (x0 + x)] = color;
+        surf.buff[((y0 - y) * surf.width) + (x0 - x)] = color;
+        surf.buff[((y0 - x) * surf.width) + (x0 - y)] = color;
+        surf.buff[((y0 + x) * surf.width) + (x0 - y)] = color;
+        surf.buff[((y0 + y) * surf.width) + (x0 - x)] = color;
+
+        if (err <= 0) {
+            y += 1;
+            err += 2 * y + 1;
+        }
+        if (err > 0) {
+            x -= 1;
+            err -= 2 * x + 1;
+        }
+    }
+
+}
+
+void draw_circle_filled(Surface& surf, u16 x0, u16 y0, u16 radius, u32 color)
+{
+    int x = radius;
+    int y = 0;
+    int err = 0;
+
+    while (x >= y) {
+        draw_line(surf, x0 - x, y0 + y, x0 + x, y0 + y, color);
+        draw_line(surf, x0 - x, y0 - y, x0 + x, y0 - y, color);
+        draw_line(surf, x0 - y, y0 + x, x0 + y, y0 + x, color);
+        draw_line(surf, x0 - y, y0 - x, x0 + y, y0 - x, color);
+
+        if (err <= 0) {
+            y += 1;
+            err += 2 * y + 1;
+        }
+        if (err > 0) {
+            x -= 1;
+            err -= 2 * x + 1;
+        }
+    }
+}
+
+void draw_rect(Surface& surf, u16 x0, u16 y0, u16 width, u16 height, u32 color)
+{
+    draw_line_horizontal(surf, x0, x0 + width, y0 ,color);
+    draw_line_horizontal(surf, x0, x0 + width, y0 - height, color);
+
+    draw_line_vertical(surf, x0, y0 - height, y0, color);
+    draw_line_vertical(surf, x0 + width, y0 - height, y0 , color);
+}
+
+void draw_rect_filled(Surface& surf, u16 x0, u16 y0, u16 width, u16 height, u32 color)
+{
+    for (int x = x0; x < x0 + width; x++)
+    {
+        for (int y = y0 - height; y < y0; y++)
+        {
+            surf.buff[y * surf.width + x] = color;
+        }
+    }
+}
+
+
